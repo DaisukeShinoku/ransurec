0d710cd855f0044284baeb4f0de9699ebb244a4b
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2025-03-20 22:08:54 +0900

    Player,Match,MatchPlayerのinsert_all系メソッドのRSpec

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".insert_all_default_players" do
+    let(:event) { create(:event) }
+    let(:number_of_players) { 5 }
+
+    it "イベントに紐付いて「選手n」という名前で選手が作成されること" do
+      described_class.insert_all_default_players(event: event, number_of_players: number_of_players)
+      players = described_class.where(event: event)
+
+      expect(players.count).to eq(number_of_players)
+      players.each_with_index do |player, index|
+        expect(player.display_name).to eq("選手#{index + 1}")
+        expect(player.event_id).to eq(event.id)
+      end
+    end
+  end
 end
