7186f02acdc7d7d863959850136bd98ca6beb84b
kenosuda4 <rs7ten@gmail.com>
2025-03-29 17:13:31 +0900

    tailwind.cssの追加とMeraki UIのコンポーネント利用

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,6 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+# tailwind読み込み定義  
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'tailwind')
