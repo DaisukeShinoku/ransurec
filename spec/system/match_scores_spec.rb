e339611e6d59ae99ea47e6e88b850c23277d1f53
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2025-04-27 12:25:46 +0900

    スコア更新機能フロントエンド実装

---
+++ spec/system/match_scores_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+# rubocop:disable all
+RSpec.describe "試合スコア入力", type: :system do
+  describe "イベント詳細ページでのスコア更新" do
+    context "ホームチームのスコアをクリックした場合" do
+      it "ダイアログからスコアを更新できること" do
+        skip "JavaScriptが絡むシステムテストの方法を調査の上で後日実装する"
+      end
+    end
+
+    context "アウェイチームのスコアをクリックした場合" do
+      it "ダイアログからスコアを更新できること" do
+        skip "JavaScriptが絡むシステムテストの方法を調査の上で後日実装する"
+      end
+    end
+  end
+end
+# rubocop:disable all
